e652194d738f033b6346a674a99254ef8e47b19e
galm <loguhiy.f@gmail.com>
2025-06-12 16:08:13 +0400

    add ct

--- Mdb.Core/Repositories/IUserRepository.cs
+++ Mdb.Core/Repositories/IUserRepository.cs
@@ -4,10 +4,10 @@ namespace Mdb.Core;
 
 public interface IUserRepository
 {
-    Task CreateAsync(User user);
-    Task UpdateAsync(User user);
-    Task DeleteAsync(Guid userId);
-    Task<List<User>> SearchAsync(string search);
-    public Task<User?> GetByAsync(Guid id);
-    IAsyncEnumerable<User>? GetAllAsync();
+    Task CreateAsync(User user, CancellationToken cancellationToken = default);
+    Task UpdateAsync(User user, CancellationToken cancellationToken = default);
+    Task DeleteAsync(Guid userId, CancellationToken cancellationToken = default);
+    Task<List<User>> SearchAsync(string search, CancellationToken cancellationToken = default);
+    public Task<User?> GetByAsync(Guid id, CancellationToken cancellationToken = default);
+    IAsyncEnumerable<User>? GetAllAsync(CancellationToken cancellationToken = default);
 }
--- Mdb.DAL/Repositories/UserRepository.cs
+++ Mdb.DAL/Repositories/UserRepository.cs
@@ -1,3 +1,4 @@
+using System.Runtime.CompilerServices;
 using Mdb.Core;
 using Mdb.Models;
 using MongoDB.Bson;
@@ -10,41 +11,41 @@ public class UserRepository(IDbFactory dbFactory) : IUserRepository
 {
     private readonly IMongoCollection<User> _users = dbFactory.GetCollection<User>("Users");
 
-    public async Task CreateAsync(User user)
+    public async Task CreateAsync(User user, CancellationToken cancellationToken = default)
     {
-        await _users.InsertOneAsync(user);
+        await _users.InsertOneAsync(user, cancellationToken: cancellationToken);
     }
 
-    public async Task UpdateAsync(User user)
+    public async Task UpdateAsync(User user, CancellationToken cancellationToken = default)
     {
-        await _users.ReplaceOneAsync(u => u.Id == user.Id, user);
+        await _users.ReplaceOneAsync(u => u.Id == user.Id, user, cancellationToken: cancellationToken);
     }
 
-    public async Task DeleteAsync(Guid userId)
+    public async Task DeleteAsync(Guid userId, CancellationToken cancellationToken = default)
     {
-        await _users.DeleteOneAsync(u => u.Id == userId);
+        await _users.DeleteOneAsync(u => u.Id == userId, cancellationToken: cancellationToken);
     }
 
-    public async IAsyncEnumerable<User>? GetAllAsync()
+    public async IAsyncEnumerable<User>? GetAllAsync([EnumeratorCancellation] CancellationToken cancellationToken = default)
     {
-        var cursor = await _users.FindAsync(new BsonDocument());
-        while (await cursor.MoveNextAsync())
+        var cursor = await _users.FindAsync(new BsonDocument(), cancellationToken: cancellationToken);
+        while (await cursor.MoveNextAsync(cancellationToken))
             foreach (var u in cursor.Current)
                 yield return u;
     }
 
-    public async Task<User?> GetByAsync(Guid id)
+    public async Task<User?> GetByAsync(Guid id, CancellationToken cancellationToken = default)
     {
-        var user = await _users.FindAsync(u => u.Id == id);
-        return await user.SingleOrDefaultAsync();
+        var user = await _users.FindAsync(u => u.Id == id, cancellationToken: cancellationToken);
+        return await user.SingleOrDefaultAsync(cancellationToken);
     }
 
-    public async Task<List<User>> SearchAsync(string search)
+    public async Task<List<User>> SearchAsync(string search, CancellationToken cancellationToken = default)
     {
         return await _users.AsQueryable()
             .Where(u =>
                 u.LastName.Contains(search, StringComparison.CurrentCultureIgnoreCase)
                 || u.FirstName.Contains(search, StringComparison.CurrentCultureIgnoreCase))
-            .ToListAsync();
+            .ToListAsync(cancellationToken);
     }
 }
